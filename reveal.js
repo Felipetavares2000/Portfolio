9cb92d51dad7b7e4fde5718a693fe08013cd7787
Felipe Tavares <103511462+Felipetavares2000@users.noreply.github.com>
2024-02-18 18:35:09 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,122 @@
+window.revelar = ScrollReveal({ reset: true })
+
+
+// TOPO DO SITE
+
+revelar.reveal('.efeito-txt-topo', 
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.titulo', 
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-box1', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-box2', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-box3', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.efeito-txt-sobre', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-img-sobre', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-port1', 
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-port2', 
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'bottom',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-port3', 
+{
+    duration: 2000,
+    distance: '90px',
+    origin: 'right',
+    delay: 1000
+})
+
+revelar.reveal('.input1', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.input2', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 800
+})
+
+revelar.reveal('.input3', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1100
+})
+
+revelar.reveal('.input4', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1400
+})
+revelar.reveal('.efeito-btn-enviar', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1700
+})
+
+
+
+
